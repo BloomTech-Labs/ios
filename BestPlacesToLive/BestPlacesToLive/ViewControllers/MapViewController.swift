789b8b246d13f5b8a95ae86f3a628382182c45ef
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-09-12 16:18:36 -0400

    Refactored so that map view is no longer accessible from Top 10 page.  Rather it is accessible from the detail view page and automatically zooms in and renders the selected city.  Removed search bar for map view.

--- BestPlacesToLive/BestPlacesToLive/ViewControllers/MapViewController.swift
+++ BestPlacesToLive/BestPlacesToLive/ViewControllers/MapViewController.swift
@@ -8,48 +8,20 @@
 import MapKit
 import UIKit
 
-class MapViewController: UIViewController, UISearchBarDelegate {
+class MapViewController: UIViewController {
     
     
-    
+    var cityName: String?
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    
-    @IBAction func searchButtonTapped(_ sender: Any) {
-        let searchController = UISearchController(searchResultsController: nil)
-        searchController.searchBar.delegate = self
-        present(searchController, animated: true, completion: nil)
-    }
-    
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        UIApplication.shared.beginIgnoringInteractionEvents()
-        
-        //Activity Indicator
-        let activityIndicator = UIActivityIndicatorView()
-        activityIndicator.style = UIActivityIndicatorView.Style.gray
-        activityIndicator.center = self.view.center
-        activityIndicator.hidesWhenStopped = true
-        activityIndicator.startAnimating()
-        
-        self.view.addSubview(activityIndicator)
-        
-        //Hide Search Bar
-        searchBar.resignFirstResponder()
-        dismiss(animated: true, completion: nil)
-        
         //Create the search request
         let searchRequest = MKLocalSearch.Request()
-        searchRequest.naturalLanguageQuery = searchBar.text
+        searchRequest.naturalLanguageQuery = cityName
         let activeSearch = MKLocalSearch(request: searchRequest)
         activeSearch.start { (response, error) in
-            
-            activityIndicator.stopAnimating()
-            UIApplication.shared.endIgnoringInteractionEvents()
             
             if response == nil {
                 NSLog("Error with search")
@@ -64,20 +36,16 @@ class MapViewController: UIViewController, UISearchBarDelegate {
                 
                 //Create annotation
                 let annotation = MKPointAnnotation()
-                annotation.title = response?.mapItems.first?.name
+                annotation.title = self.cityName
                 annotation.coordinate = CLLocationCoordinate2D(latitude: latitude!, longitude: longitude!)
                 self.mapView.addAnnotation(annotation)
                 
                 //Zooming in on annotation
                 let coordinate: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude!, longitude!)
-                let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+                let span = MKCoordinateSpan(latitudeDelta: 1.0, longitudeDelta: 1.0)
                 let region = MKCoordinateRegion(center: coordinate, span: span)
                 self.mapView.setRegion(region, animated: true)
             }
         }
-        
-        
     }
-    
-
 }
