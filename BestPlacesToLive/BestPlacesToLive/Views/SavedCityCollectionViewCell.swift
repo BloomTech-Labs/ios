e037fbe74f878db1ec14bc17bd7f87926df7742e
Lambda_School_Loaner_148 <lambda_school_loaner_148@Lambda-School-Loaner-148s-MacBook-Pro.local>
2019-09-13 19:32:27 -0400

    Added Saved Cities Page to DashBoard

---
+++ BestPlacesToLive/BestPlacesToLive/Views/SavedCityCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SavedCityCollectionViewCell.swift
+//  BestPlacesToLive
+//
+//  Created by Lambda_School_Loaner_148 on 9/13/19.
+//  Copyright © 2019 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class SavedCityCollectionViewCell: UICollectionViewCell {
+    
+}
